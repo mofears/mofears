c0357dbba3e85f8b939c12e3f004592f6f6c6da1
indinabilah <indinabilahaulia@gmail.com>
2019-07-11 23:13:20 +0700

    2nd commit to dev

--- server/app.js
+++ server/app.js
@@ -6,12 +6,15 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000
-const youtubeApi = 
+// const youtubeApi =
+const omdbApi = require('./routes/omdb') 
 
 // mongoose.connect('mongodb://localhost/HacktivGit', { useNewUrlParser: true })
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+app.use("/omdb", omdbApi)
 
 app.listen(port, () => console.log('Running on port', port))
